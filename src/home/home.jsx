a9d67670f247fefc0517e82b5c5417c93f725f96
James <jknight@lunenfeld.ca>
2017-10-18 16:52:24 -0400

    Logo
    - added main page logo

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
+import LogoImg from '../assets/logo/main-logo-for-web.png';
+
 class Home extends React.Component {
   render() {
     return (
-      <div>
-        user login
+      <div
+        style={ {
+          alignItems: 'center',
+          display: 'flex',
+          height: 'calc(100vh - 65px)',
+          justifyContent: 'center',
+          width: '100%',
+        } }
+      >
+        <img
+          alt="ScreenHits logo"
+          src={ LogoImg }
+        />
       </div>
     );
   }
